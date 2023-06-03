73252559aa429c68d928bb2ea839a3f7a9d30a14
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-06-03 13:30:34 -0500

    Added copy to clipboard to Common Utility

--- lib/Utilities/common.dart
+++ lib/Utilities/common.dart
@@ -6,4 +6,19 @@ class Common {
     FocusScope.of(context).requestFocus(FocusNode());
     SystemChannels.textInput.invokeMethod('TextInput.hide');
   }
+
+  static void copyToClipboard(BuildContext context, String text,
+      {Duration? duration}) {
+    duration ??= const Duration(seconds: 1);
+
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: duration,
+        content: const Center(
+          child: Text('Copied to Clipboard.'),
+        ),
+      ),
+    );
+  }
 }
